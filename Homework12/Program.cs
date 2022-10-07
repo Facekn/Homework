758930003be91a998dc3a4375e4001bcff2b371f
Ilya Grigorev <facekn110388@gmail.com>
2022-10-08 01:38:48 +0300

    Задание на поиск цифры с №3

---
+++ Homework12/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+
+if (num < 0)
+{
+    num = -num;
+}
+
+if (num > 100)
+{
+    while (num >= 1000)
+    {
+        num = num / 10;
+    }
+    int result = num % 10;
+    Console.WriteLine("Третья цифра числа: " + result );
+}
+else
+{
+    Console.WriteLine("Цифры с номером 3 нет в числе " +num);
+}
