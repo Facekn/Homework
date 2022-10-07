64e993cec1c99255fa6904fc5699ad0b05ca2850
Ilya Grigorev <facekn110388@gmail.com>
2022-10-07 23:00:08 +0300

    Показать вторую цифру трёхзначного числа

--- Homework8/Program.cs
+++ Homework8/Program.cs
@@ -1,5 +1,12 @@
 ﻿Console.Write("Введите трёхзначное число: ");
 int num1 = int.Parse(Console.ReadLine() ?? "0");
-int num2 = (num1 % 100);
-int num3 = (num2 / 10);
-Console.WriteLine("Вторая цифра данного числа: " +num3);
+if (num1 > -1000 && num1 < -99 || num1>99 && num1 < 1000)
+{
+    int num2 = (num1 % 100);
+    int num3 = (num2 / 10);
+    Console.WriteLine("Вторая цифра данного числа: " +num3);
+}
+else
+{
+    Console.WriteLine("Данное число не является трёхзначным");   
+}
