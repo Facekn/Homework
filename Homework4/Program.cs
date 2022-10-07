6ec584d8fbcbfcb63b460d7d2e37a8c0619b76c7
Ilya Grigorev <facekn110388@gmail.com>
2022-10-07 20:21:37 +0300

    Является ли число четным

---
+++ Homework4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+if (num % 2 == 1)
+{
+    Console.WriteLine("Число нечетное");
+}
+else
+{
+    Console.WriteLine("Число четное");
+}
