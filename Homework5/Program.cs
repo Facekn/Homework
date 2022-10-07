760d973a0de902beaa6899d04312c2fcbc06fc7e
Ilya Grigorev <facekn110388@gmail.com>
2022-10-07 20:49:36 +0300

    Показать числа от -N до N

---
+++ Homework5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите любое число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+int num1 = (number) * (-1);
+
+while (num1 <= num)
+{
+    int x = number1++;
+    Console.WriteLine(x);
+}
