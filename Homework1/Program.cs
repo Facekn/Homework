70640c570dba6e8d6ccd1f570a64ad772163b049
Ilya Grigorev <facekn110388@gmail.com>
2022-10-07 19:18:24 +0300

    Задача с квадратом числа

---
+++ Homework1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число а: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+int c = b*b;
+if (a == c)
+{
+    Console.Write("Число A является квадратом B!");
+}
+else 
+{
+    Console.Write("Число A не является квадратом B!");
+}
