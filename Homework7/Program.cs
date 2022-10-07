1de78518e7a013875b17f8ddafab2d46739598f5
Ilya Grigorev <facekn110388@gmail.com>
2022-10-07 22:11:17 +0300

    Показать третью цифру трёхзначного числа

---
+++ Homework7/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+int lastnum = (num % 10);
+Console.WriteLine("Третья цифра данного числа: " +lastnum);
