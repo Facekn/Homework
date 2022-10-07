f9404fcea61c2f4d3857928e1d62024b28aefaef
Ilya Grigorev <facekn110388@gmail.com>
2022-10-07 21:11:28 +0300

    Показать четные числа от 1 до N

---
+++ Homework6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите последнее число диапозона: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+int a = 1;
+for (int i = a; i<=b; i++)
+{
+    if(i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
