3fa6407cd26881dc65f1eedbb5a1c6850d1d9da9
Ilya Grigorev <facekn110388@gmail.com>
2022-10-07 19:39:40 +0300

    Задача с днями недели

---
+++ Homework2/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Введите номер дня недели: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+if (num == 1)
+{
+    Console.WriteLine("Сегодня Понедельник");
+}
+ else if (num == 2)
+{
+    Console.WriteLine("Сегодня Вторник");
+}
+else if (num == 3)
+{
+    Console.WriteLine("Сегодня Среда");
+}
+else if (num == 4)
+{
+    Console.WriteLine("Сегодня Четверг");
+}
+else if (num == 5)
+{
+    Console.WriteLine("Сегодня Пятница");
+}
+else if (num == 6)
+{
+    Console.WriteLine("Сегодня Суббота");
+}
+else if (num == 7)
+{
+    Console.WriteLine("Сегодня Воскресенье");
+}
+else 
+{
+    Console.WriteLine("Такого дня недели не существует");
+}
