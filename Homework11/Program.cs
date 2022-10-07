da7b36b0d81750299d391d5307e0497b81085908
Ilya Grigorev <facekn110388@gmail.com>
2022-10-08 01:23:23 +0300

    Задание на кратность числа

---
+++ Homework11/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите  число:  ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число для проверки кратности:  ");
+int provernum = int.Parse(Console.ReadLine() ?? "0");
+
+if (num % provernum == 0)
+{
+    Console.Write("Данное число кратно заданному");
+}
+else
+{
+    int num1 = num % provernum;
+    Console.Write("Данное число  не кратно заданному. Остаток будет равен:  "+num1);
+
+}
