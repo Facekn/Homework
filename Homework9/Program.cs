5e4fa3b4b9cfc1b3dda222dccb636c6fa10d9845
Ilya Grigorev <facekn110388@gmail.com>
2022-10-08 00:02:03 +0300

    Показать большую цифру числа из диапазона 10-99

---
+++ Homework9/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите число из диапазона 10 - 99:");
+int num = int.Parse(Console.ReadLine() ?? "0");
+if (num >= 10 && num <= 99)
+{
+    int a = num / 10;
+    int b = num % 10;
+    int max = a;
+    
+     if (a > b)
+    {
+        max=a;
+        Console.WriteLine("Наибольшая цифра в числе "+num+ " - " +max);
+    }
+
+    else if (a < b)
+        {
+            max = b;
+            Console.WriteLine("Наибольшая цифра в числе "+num+ " - " +max);
+        }
+
+        else
+        {
+          Console.WriteLine("Цифры в числе равны");  
+        }
+}
+
+else
+{
+    Console.WriteLine("Число не в диапазоне ");
+}
